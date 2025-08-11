020e2906d9bd6bf08ab64e45b8f7e0853d7c7287
Manu Siele <manusiele254@gmail.com>
2025-08-11 12:02:34 +0300

    Tabulation is a dynamic programming technique that solves a problem by building up the solution from the bottom, starting with the base cases.

---
+++ Dynamic_Programming/Dp with Tabulation.py
@@ -0,0 +1,19 @@
+def fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    for i in range(2, n + 1):
+        dp[i] = dp[i-1] + dp[i-2]
+    return dp[n]
+
+
+# pseudocode
+
+# F = array of length (n + 1)
+# F[0] = 0
+# F[1] = 1
+# for i from 2 to n:
+#     F[i] = F[i - 1] + F[i - 2]
